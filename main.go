1d8012a7f16c40e8639f8f9ecbb28e161d810006
CactusPuppy <coby.zhong@gmail.com>
2019-04-02 00:12:29 -0700

    Added method to store data accessibly

--- main.go
+++ main.go
@@ -8,11 +8,21 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"strconv"
 	"time"
 )
 
 const ClientID = `ari2vux13uqzdxek5b4r1vw2vg80ix`
+
+type ConfigData struct {
+	Streamer string
+	CallbackURL string
+	Port string
+	ID string
+}
+
+var configdata ConfigData
 
 func checkError(err error) {
 	if err != nil {
@@ -23,7 +33,7 @@ func checkError(err error) {
 //Performs startup tasks
 func main() {
 	start := time.Now()
-	fmt.Println("Starting TwitchGameLog v0.1 by CactusPuppy")
+	log.Println("Starting TwitchGameLog v0.1 by CactusPuppy")
 
 	//Get our http client
 	client := http.Client{}
@@ -31,43 +41,56 @@ func main() {
 	token := getToken()
 
 	//TODO: Get config values
-	streamer := "cactuspupbot"
-	callbackURL := "https://480ba225.ngrok.io"
-	port := "8080"
+	configdata.Streamer = "cactuspupbot"
+	configdata.CallbackURL = "https://480ba225.ngrok.io"
+	configdata.Port = "8080"
+
+	streamer := configdata.Streamer
+	callbackURL := configdata.CallbackURL
+	port := configdata.Port
 
 	//Get streamer ID
 	id := getStreamerID(streamer, token, client)
-	log.Println("Now tracking",streamer,"(ID:"+id+")")
+	configdata.ID = id
+	log.Println("Now tracking",streamer,"(ID:",id+")")
 
 	//Subscribe to proper webhook
 	hookURL := `https://api.twitch.tv/helix/webhooks/hub` //TODO
 	payload := map[string]interface{}{
-		"hub": map[string]string{
-			"callback":      callbackURL+"/webhook",
-			"mode":          "subscribe",
-			"topic":         "https://api.twitch.tv/helix/streams?user_id=" + id,
-			"lease_seconds": "1000",
-		},
+		"hub.callback":      callbackURL+"/webhook",
+		"hub.mode":          "subscribe",
+		"hub.topic":         "https://api.twitch.tv/helix/streams?user_id=" + id,
+		"hub.lease_seconds": "0",
+		"hub.secret":        secret.PayloadSecret,
 	}
 	payloadBytes, err := json.Marshal(payload)
 	checkError(err)
+
+	//TEMP
+	var unmarsh map[string]interface{}
+	err = json.Unmarshal(payloadBytes, &unmarsh)
+
 	//Create request
 	request, err := http.NewRequest("POST", hookURL, bytes.NewBuffer(payloadBytes))
 	checkError(err)
 	//Add auth header
 	request.Header.Set("Client-ID", ClientID)
 	request.Header.Set("Authorization", "Bearer "+token)
+	request.Header.Set("Content-Type", "application/json")
 	//Perform request
-	_, err = client.Do(request)
+	response, err := client.Do(request)
 	checkError(err)
+	if response.StatusCode != 202 {
+		log.Fatalln("Payload not accepted, HTTP code", string(response.StatusCode))
+	}
+
+	elapsed := time.Since(start)
+	log.Printf("Startup complete, took %s\n", elapsed)
 
 	//Start listening for webhook
 	http.HandleFunc("/webhook", handleHook)
 	err = http.ListenAndServe(":"+port, nil)
 	checkError(err)
-
-	elapsed := time.Since(start)
-	log.Printf("Startup complete, took %s\n", elapsed)
 }
 
 func getStreamerID(streamer string, token string, client http.Client) (id string) {
@@ -77,17 +100,21 @@ func getStreamerID(streamer string, token string, client http.Client) (id string
 	//Add auth header
 	request.Header.Set("Client-ID", ClientID)
 	request.Header.Set("Authorization", "Bearer "+token)
+
 	//Get response
 	response, err := client.Do(request)
 	checkError(err)
 	checkRateLimit(response)
 	body, err := ioutil.ReadAll(response.Body)
 	checkError(err)
+
 	//Extract response to JSON
 	var responseJson map[string]interface{}
 	err = json.Unmarshal(body, &responseJson)
 	_ = response.Body.Close()
 	checkError(err)
+	fmt.Printf("%v\n", responseJson)
+
 	//Check user actually exists
 	responseData := responseJson["data"]
 	if len(responseData.([]interface{})) == 0 {
@@ -116,11 +143,11 @@ func getToken() string {
 func checkRateLimit(response *http.Response) {
 	//TEMP: See rate limit
 	rateLimit := response.Header.Get("Ratelimit-Limit")
-	fmt.Println("Rate limit:", rateLimit)
+	log.Println("Rate limit:", rateLimit)
 	//Check remaining points
 	remainingPoints, err := strconv.Atoi(response.Header.Get("Ratelimit-Remaining"))
 	checkError(err)
-	fmt.Println("Remaining points:", remainingPoints)
+	log.Println("Remaining points:", remainingPoints)
 	if remainingPoints < 1 {
 		//Find reset time
 		i, err := strconv.ParseInt(response.Header.Get("Ratelimit-Reset"), 10, 64)
@@ -134,14 +161,21 @@ func checkRateLimit(response *http.Response) {
 
 //Handles when the webhook issues a thingy
 func handleHook(w http.ResponseWriter, r *http.Request) {
-	log.Println("Hit the hook function!")
-	log.Printf("Headers: %v\n", r.Header)
+	//TODO: Respond to query
+	if r.Method == "GET" || r.Method == "" {
+		query := r.URL.Query()
+		if query["hub.mode"][0] == "denied" {
+			log.Fatalln("Subscription to webhook was denied")
+		}
+		if !checkRequest(query) {
+			log.Fatalln("Did not get same subscription back")
+		}
 
-	body, err := ioutil.ReadAll(r.Body)
-	checkError(err)
+	}
+	//TODO: handle payload
+}
 
-	var jsonData map[string]interface{}
-	err = json.Unmarshal(body, &jsonData)
-	checkError(err)
-	log.Printf("Body: %v\n", jsonData)
+//Checks that the request is what we requested
+func checkRequest(values url.Values) bool {
+
 }
